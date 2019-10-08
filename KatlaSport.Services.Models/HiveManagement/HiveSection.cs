adda4061d051d68f378f49a9ceaf5824f596cfc5
Mikita Reut <Mikita_Reut@epam.com>
2019-10-09 01:23:27 +0300

    In the HiveSectionsController and in the HiveSectionService, implement methods for adding, editing, and deleting a HiveSection entity.

--- KatlaSport.Services.Models/HiveManagement/HiveSection.cs
+++ KatlaSport.Services.Models/HiveManagement/HiveSection.cs
@@ -5,8 +5,28 @@ namespace KatlaSport.Services.HiveManagement
     /// <summary>
     /// Represents a hive section.
     /// </summary>
-    public class HiveSection : HiveSectionListItem
+    public class HiveSection
     {
+        /// <summary>
+        /// Gets or sets a hive section identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets a hive section name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a hive section code.
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a hive is deleted.
+        /// </summary>
+        public bool IsDeleted { get; set; }
+
         /// <summary>
         /// Gets or sets a timestamp when the hive section was updated last time.
         /// </summary>
